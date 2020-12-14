09e954edfcde0d5a37c206f7c1fa43c379c9b52a
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 17:56:59 -0500

    feat(launch): Add launch fragment

---
+++ app/src/main/java/com/udacity/political/preparedness/launch/LaunchFragment.kt
@@ -0,0 +1,32 @@
+package com.udacity.political.preparedness.launch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.political.preparedness.databinding.FragmentLaunchBinding
+
+class LaunchFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentLaunchBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+
+        binding.representativeButton.setOnClickListener { navToRepresentatives() }
+        binding.upcomingButton.setOnClickListener { navToElections() }
+
+        return binding.root
+    }
+
+    private fun navToElections() {
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToElectionsFragment())
+    }
+
+    private fun navToRepresentatives() {
+        this.findNavController().navigate(LaunchFragmentDirections.actionLaunchFragmentToRepresentativeFragment())
+    }
+
+}
