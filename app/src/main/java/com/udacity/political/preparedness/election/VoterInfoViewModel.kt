306f7402ea8c4a0bda66cf14da25f726f72dac0e
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 17:55:20 -0500

    feat(election): Add voter info view model

---
+++ app/src/main/java/com/udacity/political/preparedness/election/VoterInfoViewModel.kt
@@ -0,0 +1,21 @@
+package com.udacity.political.preparedness.election
+
+import androidx.lifecycle.ViewModel
+import com.udacity.political.preparedness.database.ElectionDao
+
+class VoterInfoViewModel(private val dataSource: ElectionDao) : ViewModel() {
+
+    //TODO: Add live data to hold voter info
+
+    //TODO: Add var and methods to populate voter info
+
+    //TODO: Add var and methods to support loading URLs
+
+    //TODO: Add var and methods to save and remove elections to local database
+    //TODO: cont'd -- Populate initial state of save button to reflect proper action based on election saved status
+
+    /**
+     * Hint: The saved state can be accomplished in multiple ways. It is directly related to how elections are saved/removed from the database.
+     */
+
+}
