dd95103396bf51f8cf23b4d214b01b3dba6ff67b
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 17:55:46 -0500

    feat(election): Add voter info view model factory

---
+++ app/src/main/java/com/udacity/political/preparedness/election/VoterInfoViewModelFactory.kt
@@ -0,0 +1,8 @@
+package com.udacity.political.preparedness.election
+
+import androidx.lifecycle.ViewModelProvider
+
+//TODO: Create Factory to generate VoterInfoViewModel with provided election datasource
+class VoterInfoViewModelFactory : ViewModelProvider.Factory {
+
+}
