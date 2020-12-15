29e7d1f4b0b9e79379d87a6ef5d9bc829d5eaddc
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 22:55:59 -0500

    feat: Add election util

--- data/src/main/java/com/udacity/political/preparedness/data/util/ElectionUtil.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/util/ElectionUtil.kt
@@ -1,11 +1,11 @@
-package com.udacity.political.preparedness.network.adapter
+package com.udacity.political.preparedness.data.util
 
-/*
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import com.udacity.political.preparedness.data.storage.entity.DivisionEntity
 
-class ElectionAdapter {
+class ElectionUtil {
+
     @FromJson
     fun divisionFromJson(ocdDivisionId: String): DivisionEntity {
         val countryDelimiter = "country:"
@@ -21,4 +21,5 @@ class ElectionAdapter {
     fun divisionToJson(divisionEntity: DivisionEntity): String {
         return divisionEntity.id
     }
-}*/
+
+}
