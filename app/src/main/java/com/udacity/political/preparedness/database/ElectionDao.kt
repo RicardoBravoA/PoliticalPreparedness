12c497d714eb72d7d18edf503f3dc629d8e51614
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 20:51:08 -0500

    refactor: Update election dao

--- app/src/main/java/com/udacity/political/preparedness/database/ElectionDao.kt
+++ app/src/main/java/com/udacity/political/preparedness/database/ElectionDao.kt
@@ -1,11 +1,19 @@
 package com.udacity.political.preparedness.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.udacity.political.preparedness.network.models.Election
 
 @Dao
 interface ElectionDao {
 
     //TODO: Add insert query
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUpcomingElection(election: Election)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertSaveElection(election: Election)
 
     //TODO: Add select all election query
 
--- app/src/main/java/com/udacity/political/preparedness/network/ApiInterface.kt
+++ app/src/main/java/com/udacity/political/preparedness/network/ApiInterface.kt
@@ -1,6 +1,7 @@
 package com.udacity.political.preparedness.network
 
 import com.udacity.political.preparedness.BuildConfig
+import com.udacity.political.preparedness.network.models.Election
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,6 +11,6 @@ interface ApiInterface {
     @GET("elections")
     suspend fun feed(
         @Query("key") key: String = BuildConfig.CIVIC_API_KEY
-    ): Response<String>
+    ): Response<List<Election>>
 
 }
