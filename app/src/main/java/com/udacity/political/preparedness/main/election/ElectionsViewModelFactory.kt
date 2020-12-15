fea23c1ba40a7298f400b80ba800ab28e59820c3
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-15 01:01:51 -0500

    feat: Add election data repository

--- app/src/main/java/com/udacity/political/preparedness/main/election/ElectionsViewModelFactory.kt
+++ app/src/main/java/com/udacity/political/preparedness/main/election/ElectionsViewModelFactory.kt
@@ -1,12 +1,15 @@
 package com.udacity.political.preparedness.main.election
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ElectionsViewModelFactory : ViewModelProvider.Factory {
+@Suppress("UNCHECKED_CAST")
+class ElectionsViewModelFactory(val app: Application) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ElectionsViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
+           v al asteroidDataRepository = ElectionDataRepository(AsteroidDataStoreFactory(app))
+            val asteroidUseCase = AsteroidUseCase(asteroidDataRepository)
             return ElectionsViewModel() as T
         }
         throw IllegalArgumentException("Unable to construct view model")
