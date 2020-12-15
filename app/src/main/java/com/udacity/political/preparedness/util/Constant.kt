8e89b1cab0c8fbcf66672c17cd0162c46311fee8
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 20:19:22 -0500

    refactor: Update base url

--- app/src/main/java/com/udacity/political/preparedness/util/Constant.kt
+++ app/src/main/java/com/udacity/political/preparedness/util/Constant.kt
@@ -2,7 +2,7 @@ package com.udacity.political.preparedness.util
 
 object Constant {
 
-    const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
+    const val BASE_URL = "https://civicinfo.googleapis.com/civicinfo/v2/"
     const val DURATION = 10L
 
 }
