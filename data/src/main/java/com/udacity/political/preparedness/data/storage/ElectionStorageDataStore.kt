08a1080004e5e6bd9256bee2914c1d193a6359f8
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-15 00:21:34 -0500

    feat: Add election storage data store

---
+++ data/src/main/java/com/udacity/political/preparedness/data/storage/ElectionStorageDataStore.kt
@@ -0,0 +1,23 @@
+package com.udacity.political.preparedness.data.storage
+
+import com.udacity.political.preparedness.data.datastore.ElectionDataStore
+import com.udacity.political.preparedness.data.storage.database.ElectionDao
+import com.udacity.political.preparedness.domain.model.ElectionModel
+import com.udacity.political.preparedness.domain.model.ErrorModel
+import com.udacity.political.preparedness.domain.util.ResultType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ElectionStorageDataStore(private val electionDao: ElectionDao) : ElectionDataStore {
+
+    override suspend fun get(): ResultType<List<ElectionModel>, ErrorModel> =
+        withContext(Dispatchers.IO) {
+            try {
+                val response = electionDao.getElections()
+                return@withContext ResultType.Success(PictureMapper.transformEntityToModel(response))
+            } catch (t: Throwable) {
+                return@withContext ResultType.Error(ErrorModel())
+            }
+        }
+
+}
--- data/src/main/java/com/udacity/political/preparedness/data/storage/database/ElectionDao.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/storage/database/ElectionDao.kt
@@ -3,6 +3,7 @@ package com.udacity.political.preparedness.data.storage.database
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.udacity.political.preparedness.data.storage.entity.ElectionEntity
 
 @Dao
@@ -16,6 +17,9 @@ interface ElectionDao {
     fun insertSaveElection(election: ElectionEntity)
 
     //TODO: Add select all election query
+
+    @Query("select * from election_table")
+    fun getElections(): List<ElectionEntity>
 
     //TODO: Add select single election query
 
