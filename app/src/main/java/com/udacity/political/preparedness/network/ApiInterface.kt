0962a76e1f05fa8928ff432d25933214af48d36e
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-14 20:31:41 -0500

    feat: Update api interface

--- app/src/main/java/com/udacity/political/preparedness/network/ApiInterface.kt
+++ app/src/main/java/com/udacity/political/preparedness/network/ApiInterface.kt
@@ -1,15 +1,15 @@
 package com.udacity.political.preparedness.network
 
+import com.udacity.political.preparedness.BuildConfig
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
 
-    @GET("neo/rest/v1/feed")
+    @GET("elections")
     suspend fun feed(
-        @Query("start_date") startDate: String,
-        @Query("end_date") endDate: String
+        @Query("key") key: String = BuildConfig.CIVIC_API_KEY
     ): Response<String>
 
 }
