f8b7309e0869c555ba0c3b120570860e2cd91156
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 10:51:17 -0500

    feat: Add election detail to api interface

--- data/src/main/java/com/udacity/political/preparedness/data/network/ApiInterface.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/network/ApiInterface.kt
@@ -1,6 +1,7 @@
 package com.udacity.political.preparedness.data.network
 
 import com.udacity.political.preparedness.data.BuildConfig
+import com.udacity.political.preparedness.data.response.ElectionDetailResponse
 import com.udacity.political.preparedness.data.response.ElectionResponse
 import retrofit2.Response
 import retrofit2.http.GET
@@ -12,5 +13,11 @@ interface ApiInterface {
     suspend fun elections(
         @Query("key") key: String = BuildConfig.CIVIC_API_KEY
     ): Response<ElectionResponse>
+
+    @GET("voterinfo")
+    suspend fun electionDetail(
+        @Query("key") key: String = BuildConfig.CIVIC_API_KEY,
+        @Query("electionId") electionId: String
+    ): Response<ElectionDetailResponse>
 
 }
---
+++ data/src/main/java/com/udacity/political/preparedness/data/service/ElectionDetailServiceDataStore.kt
@@ -0,0 +1,44 @@
+package com.udacity.political.preparedness.data.service
+
+import com.udacity.political.preparedness.data.datastore.ElectionDataStore
+import com.udacity.political.preparedness.data.datastore.ElectionDetailDataStore
+import com.udacity.political.preparedness.data.response.ElectionItemResponse
+import com.udacity.political.preparedness.data.mapper.ElectionMapper
+import com.udacity.political.preparedness.data.mapper.ErrorMapper
+import com.udacity.political.preparedness.data.network.ApiManager
+import com.udacity.political.preparedness.data.storage.database.ElectionDao
+import com.udacity.political.preparedness.data.storage.database.ElectionDetailDao
+import com.udacity.political.preparedness.data.util.RetrofitErrorUtil
+import com.udacity.political.preparedness.domain.model.ElectionDetailModel
+import com.udacity.political.preparedness.domain.model.ElectionModel
+import com.udacity.political.preparedness.domain.model.ErrorModel
+import com.udacity.political.preparedness.domain.util.ResultType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class ElectionDetailServiceDataStore(private val electionDetailDao: ElectionDetailDao) : ElectionDetailDataStore {
+
+    override suspend fun get(): ResultType<List<ElectionDetailModel>, ErrorModel> {
+        val response = ApiManager.get().elections()
+        return if (response.isSuccessful) {
+            val electionResponse = response.body()
+            save(electionDao, electionResponse?.elections)
+            ResultType.Success(ElectionMapper.transformResponseToModel(electionResponse))
+        } else {
+            val error = RetrofitErrorUtil.parseError(response)!!
+            ResultType.Error(ErrorMapper.transformResponseToModel(error))
+        }
+    }
+
+    private suspend fun save(electionDao: ElectionDao, list: List<ElectionItemResponse>?) =
+        withContext(Dispatchers.IO) {
+            list?.forEach {
+                electionDao.insertUpcomingElection(
+                    ElectionMapper.transformElectionResponseToEntity(
+                        it
+                    )
+                )
+            }
+        }
+
+}
