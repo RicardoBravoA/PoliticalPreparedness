c843420c1eaebaa585a41bcad769704f346be56b
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 11:24:24 -0500

    feat: Add election detail data repository

--- data/src/main/java/com/udacity/political/preparedness/data/repository/ElectionDetailDataRepository.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/repository/ElectionDetailDataRepository.kt
@@ -1,18 +1,17 @@
 package com.udacity.political.preparedness.data.repository
 
-import com.udacity.political.preparedness.data.datastore.ElectionDataStoreFactory
+import com.udacity.political.preparedness.data.datastore.ElectionDetailDataStoreFactory
 import com.udacity.political.preparedness.domain.model.ElectionDetailModel
-import com.udacity.political.preparedness.domain.model.ElectionModel
 import com.udacity.political.preparedness.domain.model.ErrorModel
 import com.udacity.political.preparedness.domain.repository.ElectionDetailRepository
 import com.udacity.political.preparedness.domain.util.ResultType
 
-class ElectionDetailDataRepository(private val dataStoreFactory: ElectionDataStoreFactory) :
+class ElectionDetailDataRepository(private val dataStoreFactory: ElectionDetailDataStoreFactory) :
     ElectionDetailRepository {
 
     override suspend fun get(id: String): ResultType<ElectionDetailModel, ErrorModel> {
         val dataStore = dataStoreFactory.create()
-        return dataStore.get()
+        return dataStore.get(id)
     }
 
 }
