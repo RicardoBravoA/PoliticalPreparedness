7762662806f2d024744971b44f73f45ff82a31c5
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 11:23:22 -0500

    feat: Add election detail data store factory

--- data/src/main/java/com/udacity/political/preparedness/data/datastore/ElectionDetailDataStoreFactory.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/datastore/ElectionDetailDataStoreFactory.kt
@@ -2,9 +2,7 @@ package com.udacity.political.preparedness.data.datastore
 
 import android.content.Context
 import com.udacity.political.preparedness.data.service.ElectionDetailServiceDataStore
-import com.udacity.political.preparedness.data.service.ElectionServiceDataStore
 import com.udacity.political.preparedness.data.storage.ElectionDetailStorageDataStore
-import com.udacity.political.preparedness.data.storage.ElectionStorageDataStore
 import com.udacity.political.preparedness.data.storage.database.ElectionDatabase
 import com.udacity.political.preparedness.data.util.isInternet
 
@@ -15,9 +13,9 @@ class ElectionDetailDataStoreFactory(private val context: Context) {
         val value = if (context.isInternet()) Preference.CLOUD else Preference.DB
 
         return if (Preference.CLOUD == value) {
-            ElectionDetailServiceDataStore(database.electionDao)
+            ElectionDetailServiceDataStore(database.electionDetailDao)
         } else {
-            ElectionDetailStorageDataStore(database.electionDao)
+            ElectionDetailStorageDataStore(database.electionDetailDao)
         }
     }
 
