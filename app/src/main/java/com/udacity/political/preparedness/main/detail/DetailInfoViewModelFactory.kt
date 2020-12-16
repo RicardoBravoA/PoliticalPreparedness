3a94556203a93024d22b1b5b5769654bfc8788ab
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 16:57:43 -0500

    refactor: Update detail info view model factory

--- app/src/main/java/com/udacity/political/preparedness/main/detail/DetailInfoViewModelFactory.kt
+++ app/src/main/java/com/udacity/political/preparedness/main/detail/DetailInfoViewModelFactory.kt
@@ -1,13 +1,21 @@
 package com.udacity.political.preparedness.main.detail
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.udacity.political.preparedness.data.datastore.ElectionDetailDataStoreFactory
+import com.udacity.political.preparedness.data.repository.ElectionDetailDataRepository
+import com.udacity.political.preparedness.domain.usecase.ElectionDetailUseCase
 
-class DetailInfoViewModelFactory : ViewModelProvider.Factory {
+@Suppress("UNCHECKED_CAST")
+class DetailInfoViewModelFactory(val app: Application) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(DetailInfoViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return DetailInfoViewModel() as T
+
+            val dataRepository = ElectionDetailDataRepository(ElectionDetailDataStoreFactory(app))
+            val useCase = ElectionDetailUseCase(dataRepository)
+
+            return DetailInfoViewModel(useCase) as T
         }
         throw IllegalArgumentException("Unable to construct view model")
     }
