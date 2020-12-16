f25bca3ccd919a7ec1914896278d633ba91ee45e
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 00:43:47 -0500

    feat(electionServiceDataStore): Add save funtion

--- data/src/main/java/com/udacity/political/preparedness/data/service/ElectionServiceDataStore.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/service/ElectionServiceDataStore.kt
@@ -1,6 +1,7 @@
 package com.udacity.political.preparedness.data.service
 
 import com.udacity.political.preparedness.data.datastore.ElectionDataStore
+import com.udacity.political.preparedness.data.entity.ElectionItemResponse
 import com.udacity.political.preparedness.data.mapper.ElectionMapper
 import com.udacity.political.preparedness.data.mapper.ErrorMapper
 import com.udacity.political.preparedness.data.network.ApiManager
@@ -16,10 +17,21 @@ class ElectionServiceDataStore(private val electionDao: ElectionDao) : ElectionD
         val response = ApiManager.get().elections()
         return if (response.isSuccessful) {
             val electionResponse = response.body()
+            save(electionDao, electionResponse?.elections)
             ResultType.Success(ElectionMapper.transformResponseToModel(electionResponse))
         } else {
             val error = RetrofitErrorUtil.parseError(response)!!
             ResultType.Error(ErrorMapper.transformResponseToModel(error))
+        }
+    }
+
+    private fun save(electionDao: ElectionDao, list: List<ElectionItemResponse>?) {
+        list?.forEach {
+            electionDao.insertUpcomingElection(
+                ElectionMapper.transformElectionResponseToEntity(
+                    it
+                )
+            )
         }
     }
 
