f4dff01ea55854a5f280c377b8edb548f4798a16
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-16 00:19:24 -0500

    feat: Add saved election use case

--- domain/src/main/java/com/udacity/political/preparedness/domain/usecase/SavedElectionUseCase.kt
+++ domain/src/main/java/com/udacity/political/preparedness/domain/usecase/SavedElectionUseCase.kt
@@ -5,7 +5,7 @@ import com.udacity.political.preparedness.domain.model.ErrorModel
 import com.udacity.political.preparedness.domain.repository.ElectionRepository
 import com.udacity.political.preparedness.domain.util.ResultType
 
-class ElectionOfflineUseCase(private val electionRepository: ElectionRepository) {
+class SavedElectionUseCase(private val electionRepository: ElectionRepository) {
 
     suspend fun get(): ResultType<List<ElectionModel>, ErrorModel> {
         return electionRepository.get()
