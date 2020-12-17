09a9c66d235c111c6679129bdba82ee0385252e6
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-17 00:12:54 -0500

    feat: Add geocode and voter info functions in api interface

--- data/src/main/java/com/udacity/political/preparedness/data/network/ApiInterface.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/network/ApiInterface.kt
@@ -3,9 +3,11 @@ package com.udacity.political.preparedness.data.network
 import com.udacity.political.preparedness.data.BuildConfig
 import com.udacity.political.preparedness.data.response.ElectionDetailResponse
 import com.udacity.political.preparedness.data.response.ElectionResponse
+import com.udacity.political.preparedness.data.util.Constant
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface ApiInterface {
 
@@ -18,6 +20,20 @@ interface ApiInterface {
     suspend fun electionDetail(
         @Query("electionId") electionId: String,
         @Query("key") key: String = BuildConfig.CIVIC_API_KEY
+    ): Response<ElectionDetailResponse>
+
+    @GET("voterinfo")
+    suspend fun voterInfo(
+        @Query("address") address: String,
+        @Query("electionId") electionId: String,
+        @Query("key") key: String = BuildConfig.CIVIC_API_KEY
+    ): Response<ElectionDetailResponse>
+
+    @GET
+    suspend fun geocode(
+        @Query("latlng") latLng: String,
+        @Query("key") key: String = BuildConfig.CIVIC_API_KEY,
+        @Url url: String = Constant.GEOCODE_URL
     ): Response<ElectionDetailResponse>
 
 }
--- data/src/main/java/com/udacity/political/preparedness/data/util/Constant.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/util/Constant.kt
@@ -3,6 +3,7 @@ package com.udacity.political.preparedness.data.util
 object Constant {
 
     const val BASE_URL = "https://civicinfo.googleapis.com/civicinfo/v2/"
+    const val GEOCODE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
     const val DURATION = 10L
 
 }
