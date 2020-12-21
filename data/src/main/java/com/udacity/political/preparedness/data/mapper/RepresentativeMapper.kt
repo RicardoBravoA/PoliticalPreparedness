d2c60ad2b8b5ec187f8a6e08120131d698ac1ab1
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-20 23:38:37 -0500

    feat: Add representative mapper

--- data/src/main/java/com/udacity/political/preparedness/data/mapper/RepresentativeMapper.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/mapper/RepresentativeMapper.kt
@@ -1,12 +1,11 @@
 package com.udacity.political.preparedness.data.mapper
 
-import com.udacity.political.preparedness.data.response.geocode.GeocodeResponse
 import com.udacity.political.preparedness.data.response.representative.RepresentativeResponse
-import com.udacity.political.preparedness.domain.model.GeocodeModel
+import com.udacity.political.preparedness.domain.model.representative.RepresentativeModel
 
 object RepresentativeMapper {
 
-    fun transformResponseToModel(representativeResponse: RepresentativeResponse): GeocodeModel {
+    fun transformResponseToModel(representativeResponse: RepresentativeResponse): RepresentativeModel {
         return GeocodeModel(geocodeResponse.results.first().address)
     }
 }
