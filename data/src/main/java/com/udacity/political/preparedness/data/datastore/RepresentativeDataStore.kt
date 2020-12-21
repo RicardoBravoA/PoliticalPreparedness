02a166c10934c9e33c36a60df7922365c9324908
Ricardo Bravo <ricardobravo@outlook.com>
2020-12-21 00:07:47 -0500

    feat: Add representative service data store

--- data/src/main/java/com/udacity/political/preparedness/data/datastore/RepresentativeDataStore.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/datastore/RepresentativeDataStore.kt
@@ -1,11 +1,11 @@
 package com.udacity.political.preparedness.data.datastore
 
-import com.udacity.political.preparedness.data.response.representative.RepresentativeResponse
 import com.udacity.political.preparedness.domain.model.ErrorModel
+import com.udacity.political.preparedness.domain.model.representative.RepresentativeModel
 import com.udacity.political.preparedness.domain.util.ResultType
 
 interface RepresentativeDataStore {
 
-    suspend fun get(): ResultType<RepresentativeResponse, ErrorModel>
+    suspend fun get(address: String): ResultType<RepresentativeModel, ErrorModel>
 
 }
--- data/src/main/java/com/udacity/political/preparedness/data/service/RepresentativeServiceDataStore.kt
+++ data/src/main/java/com/udacity/political/preparedness/data/service/RepresentativeServiceDataStore.kt
@@ -2,20 +2,20 @@ package com.udacity.political.preparedness.data.service
 
 import com.udacity.political.preparedness.data.datastore.RepresentativeDataStore
 import com.udacity.political.preparedness.data.mapper.ErrorMapper
-import com.udacity.political.preparedness.data.mapper.GeocodeMapper
+import com.udacity.political.preparedness.data.mapper.RepresentativeMapper
 import com.udacity.political.preparedness.data.network.ApiManager
-import com.udacity.political.preparedness.data.response.representative.RepresentativeResponse
 import com.udacity.political.preparedness.data.util.RetrofitErrorUtil
 import com.udacity.political.preparedness.domain.model.ErrorModel
+import com.udacity.political.preparedness.domain.model.representative.RepresentativeModel
 import com.udacity.political.preparedness.domain.util.ResultType
 
 class RepresentativeServiceDataStore() : RepresentativeDataStore {
 
-    override suspend fun get(): ResultType<RepresentativeResponse, ErrorModel> {
-        val response = ApiManager.get().geocode(latLng = coordinates)
+    override suspend fun get(address: String): ResultType<RepresentativeModel, ErrorModel> {
+        val response = ApiManager.get().representatives(address)
         return if (response.isSuccessful) {
             val response = response.body()
-            ResultType.Success(GeocodeMapper.transformResponseToModel(response!!))
+            ResultType.Success(RepresentativeMapper.transformResponseToModel(response!!))
         } else {
             val error = RetrofitErrorUtil.parseError(response)!!
             ResultType.Error(ErrorMapper.transformResponseToModel(error))
